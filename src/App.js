5378fba1eb928369172e6e6464f25600f23ec36d
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-08-25 17:59:59 -0400

    feat: cria estrutura inicial da interface Athlete DB

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      ola mundo
+    <div className='container'>
+      <div
+        className='mt-3 justify-content-center align-items-center mx-auto'
+        style={{"width": "70vw", "backgroundColor": "#ffffff"}}
+      >
+        <h2 className='text-center text-white bg-success card mb-1'>Athlete DB</h2>
+
+        <h6 className='card text-center text-white bg-success mb-1 pb-1'>Informações de Jogadores</h6>
+      
+      </div>
     </div>
   );
 }
