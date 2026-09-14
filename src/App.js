1dd7642fecb738fa5223e3323f941fa7f4810fc3
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-09-14 19:47:46 -0400

    feat: adiciona formulário de cadastro de jogadores e rodapé

--- src/App.js
+++ src/App.js
@@ -9,8 +9,22 @@ function App() {
       >
         <h2 className='text-center text-white bg-success card mb-1'>Athlete DB</h2>
 
-        <h6 className='card text-center text-white bg-success mb-1 pb-1'>Informações de Jogadores</h6>
+        <h6 className='card text-center text-white bg-success mb-2 pb-1'>Informações de Jogadores</h6>
       
+        <div className='card-body text-center'>
+          <h5 className='card text-center text-white bg-dark mb-2 pb-1'>Cadastro Jogador</h5>
+          <span className='card-text'>
+            <input className='mb-2 form-control' placeholder='Informe o nome'/>
+            <input className='mb-2 form-control' placeholder='Informe a idade'/>
+            <input className='mb-2 form-control' placeholder='Informe o time'/>
+            <button className='btn btn-outline-success mb-4'>Cadastrar</button>
+          </span>
+          <h5 className='card text-center text-white bg-dark mb-4 pb-1'>Lista de Jogadores</h5>
+          <div>
+
+          </div>
+        </div>
+        <h6 className='card text-center text-light bg-success pb-1'>&copy; Thamires Sarges - 2026</h6>
       </div>
     </div>
   );
