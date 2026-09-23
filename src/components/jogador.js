829bd3735bccfff8e575826b713b29fdd93c673a
Thamires Sarges <thamires.sarges.58@gmail.com>
2026-09-23 13:18:37 -0400

    feat: adiciona exclusão de jogador com requisição DELETE

---
+++ src/components/jogador.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+
+function Jogador(props){
+
+    const excluiJogador = (id) => {
+        axios.delete(`http://127.0.0.1:8000/jogadores/${id}`)
+        .then(res => {
+            alert('Jogador removido com sucesso')
+        })
+    }
+
+    return(
+        <div>
+            <p>
+                <span className="fw-bold">
+                    {props.jogador.nome} - {props.jogaodr.idade} - {props.jogador.time}
+                </span>
+                <button 
+                    onClick={excluiJogador}
+                    className="btn btn-danger"
+                >
+                    <span className="badge rounded-pill bg-danger">X</span>
+                </button>
+            </p>
+        </div>
+    )
+}
+
+export default Jogador;
